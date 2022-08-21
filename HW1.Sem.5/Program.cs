40f42a0134f5b8dafe86e35931e144754e3a7ff7
yakov kuzin <kuzin_js@mail.ru>
2022-08-21 21:25:15 +0300

    Init commit. Task1.1

--- HW1.Sem.5/Program.cs
+++ HW1.Sem.5/Program.cs
@@ -1,1 +1,24 @@
-﻿
+﻿// a*=a умножение числа на само себя
+// a*=a+b сначала сложение, потом умноженние на а
+
+// Задайте одномерный массив из 123 случайных чисел. Найти кол-во элементов
+// массива, значения которых в отрезке от 10 до 99
+
+int[] array = new int[123];
+int i = 0;
+int count = 0;
+for (i = 0; i < array.Length-1; i++)
+{
+    array[i] = new Random().Next(0, 124);
+    array[array.Length-1]= new Random().Next(0, 124);
+    Console.Write(array[i] + ", ");
+}
+Console.WriteLine(array[array.Length-1] + ".");
+for (i = 0; i < array.Length; i++)
+{
+    if (array[i] <= 99 && array[i] >= 10)
+    {
+        count++;
+    }
+}
+Console.WriteLine(" Количество чисел от 10 до 99 в массиве = " + count);
