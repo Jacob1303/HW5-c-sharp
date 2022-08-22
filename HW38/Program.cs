2a1e06085f099cf864cd2c9cb11f21a19926a618
yakov kuzin <kuzin_js@mail.ru>
2022-08-22 12:38:14 +0300

    HW38 added

---
+++ HW38/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задайте массив вещественных чисел. Найдите разницу между максимальным и минимальным элементов массива.
+
+using System;
+
+class Programm
+{
+    static void CreatArray(double[] numbers)
+    {
+        for (int i = 0; i < numbers.Length - 1; i++)
+        {
+            numbers[i] = new Random().Next(-50, 50);
+            Console.Write(numbers[i] + "; ");
+        }
+        numbers[numbers.Length - 1] = new Random().Next(-50, 50);
+        Console.WriteLine(numbers[numbers.Length - 1] + ".");
+    }
+   
+    static void Main(string[] args)
+    {
+        double[] array = new double[10];
+        CreatArray(array);
+        double SubtMaxMin = array.Max() - array.Min();
+        Console.WriteLine("Разница между максимальным и минимальным значениями массива = " + SubtMaxMin);
+    }
+
+}
