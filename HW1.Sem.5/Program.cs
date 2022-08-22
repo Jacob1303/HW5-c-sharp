550936f4b9d2c53aeeffd068d0c0cb26cc4427ee
yakov kuzin <kuzin_js@mail.ru>
2022-08-22 11:33:24 +0300

    Fixed bugs

--- HW1.Sem.5/Program.cs
+++ HW1.Sem.5/Program.cs
@@ -1,24 +1,41 @@
-﻿// a*=a умножение числа на само себя
-// a*=a+b сначала сложение, потом умноженние на а
+﻿
+// Задайте одномерный массив из 123 случайных чисел. Найти кол-во элементов массива, значения которых в отрезке от 10 до 99
 
-// Задайте одномерный массив из 123 случайных чисел. Найти кол-во элементов
-// массива, значения которых в отрезке от 10 до 99
+using System;
 
-int[] array = new int[123];
-int i = 0;
-int count = 0;
-for (i = 0; i < array.Length-1; i++)
+class Programm
 {
-    array[i] = new Random().Next(0, 124);
-    array[array.Length-1]= new Random().Next(0, 124);
-    Console.Write(array[i] + ", ");
-}
-Console.WriteLine(array[array.Length-1] + ".");
-for (i = 0; i < array.Length; i++)
-{
-    if (array[i] <= 99 && array[i] >= 10)
+
+    static void CreatArray(int[] array)
     {
-        count++;
+        int i = 0;
+        for (i = 0; i < array.Length - 1; i++)
+        {
+            array[i] = new Random().Next(0, 124);
+            array[array.Length - 1] = new Random().Next(0, 124);
+            Console.Write(array[i] + ", ");
+        }
+        Console.WriteLine(array[array.Length - 1] + ".");
     }
+
+    static void CheckArray(int[] array)
+    {
+        int count = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            if (array[i] <= 99 && array[i] >= 10)
+            {
+                count++;
+            }
+        }
+        Console.WriteLine("Количество чисел от 10 до 99 в массиве = " + count);
+    }
+
+    static void Main(string[] args)
+    {
+        int[] array = new int[123];
+        CreatArray(array);
+        CheckArray(array);
+    }
+
 }
-Console.WriteLine(" Количество чисел от 10 до 99 в массиве = " + count);
--- HW2.Sem.5/Program.cs
+++ HW2.Sem.5/Program.cs
@@ -1,37 +1,48 @@
 ﻿// Найдите произведение пар чисел в одномерном массиве. Парой считаем первый и последний элемент, второй и предпоследний и т.д. Результат запишите в новом массиве.
 
-Console.WriteLine("Введите кол-во элеметов массива. Массив будет состоять из положительных целых чисел, полученных в случайном порядке, в интервале от 1 до 30");
-int[] array = new int[Convert.ToInt32(Console.ReadLine())];
-Console.Write("Ваш массив ");
-for (int i = 0; i < array.Length - 1; i++)
-{
-    array[i] = new Random().Next(1, 30);
-    array[array.Length - 1] = new Random().Next(1, 30);
-    Console.Write(array[i] + ", ");
-}
-Console.WriteLine(array[array.Length - 1] + ".");
+using System;
 
-int flag = array.Length % 2;
+class Programm
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Введите кол-во элеметов массива. Массив будет состоять из положительных целых чисел, полученных в случайном порядке, в интервале от 1 до 10");
+        int[] array = new int[Convert.ToInt32(Console.ReadLine())];
+        Console.Write("Ваш массив ");
+        for (int i = 0; i < array.Length - 1; i++)
+        {
+            array[i] = new Random().Next(1, 11);
+            array[array.Length - 1] = new Random().Next(1, 11);
+            Console.Write(array[i] + ", ");
+        }
+        Console.WriteLine(array[array.Length - 1] + ".");
 
-if (flag == 0)
-{
-    int[] array1 = new int[array.Length / 2];
-    Console.Write("Результат перемножения элементов массива ");
-    for (int i = 0; i < array1.Length; i++)
-    {
-        array1[i] = array[i] * array[array.Length - 1 - i];
-        Console.Write(array1[i] + ", ");
-    };
-}
-else
-{
-    int[] array1 = new int[array.Length / 2 + 1];
-    Console.Write("Результат перемножения элементов массива ");
-    for (int i = 0; i < array.Length / 2; i++)
-    {
-        array1[i] = array[i] * array[array.Length - 1 - i];
-        array1[array.Length / 2] = array[array.Length / 2];
-        Console.Write(array1[i] + ", ");
-    };
-    Console.WriteLine(array1[array.Length / 2] + ".");
+        int flag = array.Length % 2;
+
+        if (flag == 0)
+        {
+            int[] array1 = new int[array.Length / 2];
+            Console.Write("Результат перемножения элементов массива ");
+            for (int i = 0; i < array1.Length - 1; i++)
+            {
+                array1[i] = array[i] * array[array.Length - 1 - i];
+                array1[array1.Length - 1] = array[array.Length / 2 - 1] * array[array.Length / 2];
+                Console.Write(array1[i] + ", ");
+            };
+            Console.WriteLine(array1[array1.Length - 1] + ".");
+        }
+        else
+        {
+            int[] array1 = new int[array.Length / 2 + 1];
+            Console.Write("Результат перемножения элементов массива ");
+            for (int i = 0; i < array.Length / 2; i++)
+            {
+                array1[i] = array[i] * array[array.Length - 1 - i];
+                array1[array.Length / 2] = array[array.Length / 2];
+                Console.Write(array1[i] + ", ");
+            };
+            Console.WriteLine(array1[array.Length / 2] + ".");
+        }
+    }
+
 }
